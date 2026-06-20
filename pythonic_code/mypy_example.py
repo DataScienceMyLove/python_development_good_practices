fad3ae787246195ddda577eb92a46cc684c3d52a
DataScienceMyLove <149274153+DataScienceMyLove@users.noreply.github.com>
2026-06-20 13:25:21 +0000

    update mypy_example.py with docstrings & module treatment

--- pythonic_code/mypy_example.py
+++ pythonic_code/mypy_example.py
@@ -1,6 +1,14 @@
+"""
+This is my app!!!
+"""
+
 def hello(name: str,age: int) -> str:
+    """
+    This function takes a name and an age as input and returns a greeting message.
+    """
     return f"Hello {name}, you are {age} years old!"
 
-print(hello("Alice", 30))
-print(hello("Bob", "25"))
-print(hello(123,"Bob")) 
+if __name__ == "__main__":
+    print(hello("Alice", 30))
+    print(hello("Bob", 25))
+    print(hello("Charlie", 35))
